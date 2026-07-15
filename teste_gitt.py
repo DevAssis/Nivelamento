fd30f6680670a72c82feb7208eea2756d3dd0933
Milton Assis <milton.assis@crefaz.com>
2026-07-15 16:20:56 -0300

    feat: primeir commit criação script e xlsx

---
+++ teste_gitt.py
@@ -0,0 +1,7 @@
+import sqlite3
+import pandas as pd
+from openpyxl import load_workbook
+from sqlalchemy import create_engine
+
+
+db = pd.read_excel('D:/Nivelamento/Planilha.xlsx', sheet_name='Sheet1')
