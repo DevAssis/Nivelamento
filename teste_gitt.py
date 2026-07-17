20326fb7540293258efa85ae2ddcfd5252c3b9ac
Milton Assis <milton.assis@crefaz.com>
2026-07-16 21:13:43 -0300

    feat:adcionando gitignore e uma alteração esquecida

--- teste_gitt.py
+++ teste_gitt.py
@@ -4,5 +4,5 @@ from openpyxl import load_workbook
 from sqlalchemy import create_engine
 
 
-db = pd.read_excel('D:/Nivelamento/Planilha.xlsx', sheet_name='Sheet1')
+db = pd.read_excel(r'D:\Nivelamento\Planilhas.xlsx', sheet_name='Sheet1')
 print(db)
