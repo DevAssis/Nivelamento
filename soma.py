95f36e5b9499e5a2772886852bcb9db741008d8e
Milton Assis <milton.assis@crefaz.com>
2026-07-29 23:05:31 -0300

    feat:adicioanando novo codigo

---
+++ soma.py
@@ -0,0 +1,12 @@
+print("Hello, World!")
+print("vamos aprender a usar o git")
+
+
+def soma(a, b):
+    return a + b
+
+
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+resultado = soma(numero1, numero2)
+print("O resultado da soma é:", resultado)
--- teste_gitt.py
+++ teste_gitt.py
@@ -5,4 +5,4 @@ from sqlalchemy import create_engine
 
 
 db = pd.read_excel(r'D:\Nivelamento\Planilhas.xlsx', sheet_name='Sheet1')
-print(db)
+print(db.sheet_names)
