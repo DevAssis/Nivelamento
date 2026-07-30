1dacffe46199a7fd53e6dd5c79e036987d19265b
Milton Assis <milton.assis@crefaz.com>
2026-07-30 11:17:23 -0300

    feat: criado script de test

---
+++ test_soma.py
@@ -0,0 +1,12 @@
+print("Hello, World!")
+print("vamos aprender a usar o git")
+
+a=2
+b=3
+def soma(a, b):
+    return a + b
+
+
+
+resultado = soma(a, b)
+print("O resultado da soma vai ser esse:", resultado)
