c141abf150c5e7adb5f28c74887b5faaa5a76a06
Milton Assis <milton.assis@crefaz.com>
2026-07-30 01:33:02 -0300

    feat:tirei a letra

--- soma.py
+++ soma.py
@@ -9,4 +9,4 @@ def soma(a, b):
 numero1 = int(input("Digite o primeiro número: "))
 numero2 = int(input("Digite o segundo número: "))
 resultado = soma(numero1, numero2)
-print("O resultado da soma é:", resultado)
+print("O resultado da soma:", resultado)
