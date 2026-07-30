5cd504520c9efbd9f9b9a1344e97265f84b0d8c3
Milton Assis <milton.assis@crefaz.com>
2026-07-30 00:26:47 -0300

    feat: adicionado dois prints

--- teste_git.py
+++ teste_git.py
@@ -1,2 +1,4 @@
 print("Hello, World!")
 print("This is a test file for Git.")
+print("Let's learn how to use Git effectively.")
+print("This file will be used to demonstrate Git commands and workflows.")
